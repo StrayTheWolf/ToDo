7fc90ed3b8006cc7b798eff2e10cabd950c0f3a3
Wolf Stanislav <ork2004@yandex.ru>
2021-04-09 23:31:32 +0300

    layout next

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,24 @@
 new Vue({
     el: '#app',
     data: {
-        users: [
-            { id: 1, name: 'Иван'},
-            { id: 2, name: 'Иван'}
+        tasks: [
+            {id: '1', name: 'Задача 1', time: '12 pm'},
+            {id: '2', name: 'Задача 2', time: '11 pm'},
+            {id: '3', name: 'Задача 3', time: '10 pm'},
+            {id: '4', name: 'Задача 4', time: '9 pm'},
+            {id: '5', name: 'Задача 5', time: '8 pm'}
         ],
-        message: 'Meet Lorence',
-        time: '12 pm',
         description: '',
-        status: true
+        status: true,
+
+    },
+    methods: {
+        openNewTaskWindow() {
+            document.getElementById('todo-add').style.display = 'flex'
+        },
+
+        openTask() {
+            document.getElementById('todo-description').style.display = 'flex'
+        }
     }
-})
+});
---
+++ scripts/storage.js
@@ -0,0 +1,25 @@
+export class LocalStorage {
+
+    constructor(id, taskName, taskTime, description) {
+        this.id = id;
+        this.taskName = taskName;
+        this.taskName = taskName;
+        this.description = description
+    }
+
+    add(key, value) {
+        localStorage.setItem(key, value)
+    }
+
+    get(key) {
+        localStorage.getItem(key)
+    }
+
+    delete(key) {
+        localStorage.removeItem(key)
+    }
+
+    update(key, value) {
+        localStorage.setItem(key, value)
+    }
+}
