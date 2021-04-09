f2143253d84fc66246701eac25cd0f559113609f
Wolf Stanislav <ork2004@yandex.ru>
2021-04-09 17:37:05 +0300

    layout

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+        users: [
+            { id: 1, name: 'Иван'},
+            { id: 2, name: 'Иван'}
+        ],
+        message: 'Meet Lorence',
+        time: '12 pm',
+        description: '',
+        status: true
+    }
+})
